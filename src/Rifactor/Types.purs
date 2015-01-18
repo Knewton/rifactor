9a9ef2b9d4c07bf601c28555ac568a16ac1136c2
Tim Dysinger <tim@dysinger.net>
2015-01-17 18:10:24 -1000

    remove purescript from the project

--- src/Rifactor/Types.purs
+++
@@ -1,7 +0,0 @@
-module Rifactor.Types where
-
-type AwsCfg = { accounts :: [AwsAccountCfg] }
-
-type AwsAccountCfg = { name :: String
-                     , access_key_id :: String
-                     , secret_access_key :: String }
--- test/Test/Main.purs
+++
@@ -1,9 +0,0 @@
-module Test.Main where
-
-import Debug.Trace
-import Test.QuickCheck
-
-main = do
-
-  trace "(+) should add two numbers and give their sum"
-  quickCheck $ \n -> n + n == 2 * n <?> show n ++ " + " ++ show n ++ " failed"
