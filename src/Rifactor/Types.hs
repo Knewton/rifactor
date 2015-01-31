874977c4876613bc17ae31ff56ec5b7186c810e8
Tim Dysinger <tim@dysinger.net>
2015-01-30 15:45:05 -1000

    Added comments & shuffled things a little

--- src/Rifactor/Types.hs
+++ src/Rifactor/Types.hs
@@ -23,26 +23,67 @@ import           Network.AWS.Data (toText)
 import           Network.AWS.EC2.Types hiding (Region)
 import           Rifactor.Types.Internal (deriveOptions)
 
+-- | Our runtime Options for planning any EC2 Reserved Instance
+-- movement.  We need a JSON file path with the configuration in it,
+-- we need to know whether to do a dry run or not and finally we need
+-- to know if we should be verbose & spit out all the Amazon API call
+-- data & rq/rs information.
 data Options =
   Options {_file :: FilePath
           ,_dry :: Bool
           ,_verbose :: Bool }
 
+-- | A Record to represet an Account (Amazon Account). This record
+-- type translates to/from JSON in the config file.  It gives us our
+-- IAM credentials with which to run.
 data Account =
   Account {_name :: String
           ,_accessKey :: String
           ,_secretKey :: String}
 
+-- | Config is a combination of accounts & regions we want to cover
+-- for all the accounts.  It looks like so:
+-- {
+--   "accounts": [
+--     {
+--       "access_key": "AKIAIAD9VZRWT1NS2A7Q",
+--       "secret_key": "/0wyT2f1cE3d9mcRWokc2avHqaQY79onabdgZqDv",
+--       "name": "production"
+--     },
+--     {
+--       "access_key": "AKIAJV7QP3BA6NHTZJCQ",
+--       "secret_key": "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",
+--       "name": "staging"
+--     },
+--     {
+--       "access_key": "AKIAJMLRD3JB74IJIN7A",
+--       "secret_key": "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",
+--       "name": "development"
+--     }
+--   ],
+--   "regions": [
+--     "NorthCalifornia",
+--     "NorthVirginia",
+--     "Oregon"
+--   ]
+-- }
 data Config =
   Config {_accounts :: [Account]
          ,_regions :: [Region]}
 
+-- | We need an instance of Eq so our derived instance of Eq for
+-- Reserved & OnDemand will work.  Nevermind "orphaned instance"
+-- errors. We need this or else we have to implement our Eq instances
+-- for our Model by hand.
+instance Eq Env
+
+-- | OnDemand represents Instancs that we haven't found a match (with
+-- ReservedInstances) yet.  We can also track the Env that we got the
+-- instance from in the record.
 data OnDemand =
   OnDemand {_odEnv :: Env
            ,_odInstance :: Instance}
   deriving (Eq)
-
-instance Eq Env
 
 data Reserved
   = Reserved {_reEnv :: Env
